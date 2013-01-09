f8511b8bcce5fe7c3459ce3b5a5cca2fd4814969
abhinav narain <nabhinav3@gatech.edu>
2013-01-09 17:26:00 +0000

    changes in folder names and name of binary in Makefile

--- address_table.h
+++ address_table.h
@@ -11,21 +11,21 @@
 #define MAC_TABLE_DATA_ERR_ENTRIES 4096
 #define MAC_TABLE_CTL_ERR_ENTRIES 2048
 
-#define UPDATE_FILENAME_DIGEST "/tmp/bismark-uploads/wifi_beacons/%s-%" PRIu64 "-digest-%d.gz"
-#define UPDATE_MGMT_FILENAME    "/tmp/bismark-uploads/wifi_beacons/%s-%" PRIu64 "-m-%d-%c.gz"
-#define UPDATE_CONTROL_FILENAME "/tmp/bismark-uploads/wifi_beacons/%s-%" PRIu64 "-c-%d-%c.gz"
-#define UPDATE_DATA_FILENAME    "/tmp/bismark-uploads/wifi_beacons/%s-%" PRIu64 "-d-%d-%c.gz"
-#define UPDATE_PHY_FILENAME    "/tmp/bismark-uploads/wifi_beacons/%s-%" PRIu64 "-p-%d-%c.gz"
+#define UPDATE_FILENAME_DIGEST "/tmp/bismark-uploads/wifi_dump/%s-%" PRIu64 "-digest-%d.gz"
+#define UPDATE_MGMT_FILENAME    "/tmp/bismark-uploads/wifi_dump/%s-%" PRIu64 "-m-%d-%c.gz"
+#define UPDATE_CONTROL_FILENAME "/tmp/bismark-uploads/wifi_dump/%s-%" PRIu64 "-c-%d-%c.gz"
+#define UPDATE_DATA_FILENAME    "/tmp/bismark-uploads/wifi_dump/%s-%" PRIu64 "-d-%d-%c.gz"
+#define UPDATE_PHY_FILENAME    "/tmp/bismark-uploads/wifi_dump/%s-%" PRIu64 "-p-%d-%c.gz"
 
-#define UPDATE_FILENAME_COUNTS "/tmp/bismark-uploads/wifi_beacons/%s-%" PRIu64 "-co-%d.gz"
+#define UPDATE_FILENAME_COUNTS "/tmp/bismark-uploads/wifi_dump/%s-%" PRIu64 "-co-%d.gz"
 
-#define PENDING_UPDATE_MGMT_FILENAME "/tmp/wifi_beacons/current-mgmt-update-%c.gz"
-#define PENDING_UPDATE_CONTROL_FILENAME "/tmp/wifi_beacons/current-control-update-%c.gz"
-#define PENDING_UPDATE_DATA_FILENAME "/tmp/wifi_beacons/current-data-update-%c.gz"
-#define PENDING_UPDATE_PHY_FILENAME "/tmp/wifi_beacons/current-phy-update-%c.gz"
-#define PENDING_UPDATE_FILENAME_DIGEST "/tmp/wifi_beacons/current-digest-update-%c.gz"
+#define PENDING_UPDATE_MGMT_FILENAME "/tmp/wifi_dump/current-mgmt-update-%c.gz"
+#define PENDING_UPDATE_CONTROL_FILENAME "/tmp/wifi_dump/current-control-update-%c.gz"
+#define PENDING_UPDATE_DATA_FILENAME "/tmp/wifi_dump/current-data-update-%c.gz"
+#define PENDING_UPDATE_PHY_FILENAME "/tmp/wifi_dump/current-phy-update-%c.gz"
+#define PENDING_UPDATE_FILENAME_DIGEST "/tmp/wifi_dump/current-digest-update-%c.gz"
 
-#define PENDING_UPDATE_COUNTS_FILENAME "/tmp/wifi_beacons/current-count-update.gz"
+#define PENDING_UPDATE_COUNTS_FILENAME "/tmp/wifi_dump/current-count-update.gz"
 
 
 #define HOMESAW_RX_FRAME_HEADER 58
