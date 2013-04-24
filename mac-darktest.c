b5e8dcdc24cfd06c772f4afacc13dbb8a2fc1100
abhinav narain <nabhinav3@gatech.edu>
2013-04-24 10:08:52 +0000

    changing buffer size

--- mac-darktest.c
+++ mac-darktest.c
@@ -885,7 +885,7 @@ int main(int argc, char *argv[]){
     return 1;
   }
 										 
- pcap_handle = pcap_open_live(argv[1], BUFSIZ, PCAP_PROMISCUOUS, PCAP_TIMEOUT_MILLISECONDS, errbuf);
+ pcap_handle = pcap_open_live(argv[1], BUFSIZ/2, PCAP_PROMISCUOUS, PCAP_TIMEOUT_MILLISECONDS, errbuf);
   if (!pcap_handle) {
     fprintf(stderr, "Couldn't open device %s: %s\n", argv[1], errbuf);
     return NULL;
